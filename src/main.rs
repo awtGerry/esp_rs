d127acf71c22a489d3e3ac59a3abaa658353ec84
Victor Rodriguez <awtgerry@gmail.com>
2023-06-21 20:01:47 -0600

    initial script

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+#![allow(unused_imports)]
+
+use esp_idf_hal::{
+    delay::FreeRtos,
+    gpio::{IOPin, PinDriver},
+    peripherals::Peripherals,
+};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use log::*;
+use esp_println::println;
+// use log::*;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -8,5 +16,17 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Hello, world!");
+    // info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led_pin = PinDriver::output(peripherals.pins.gpio8).unwrap();
+
+    loop {
+        led_pin.set_low().unwrap();
+        println!("LED ON");
+        FreeRtos::delay_ms(1000);
+        led_pin.set_high().unwrap();
+        println!("LED OFF");
+        FreeRtos::delay_ms(1000);
+    }
 }
